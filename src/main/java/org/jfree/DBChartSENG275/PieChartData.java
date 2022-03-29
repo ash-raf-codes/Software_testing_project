e9c2b31422b02570cd6ddfd88b0f5e16dc5cb6ac
minhn <minhn@uvic.ca>
2022-03-29 08:34:00 -0700

    Minh's coding. Note that PieChartData no longer use Connecting class.

--- src/main/java/org/jfree/DBChartSENG275/PieChartData.java
+++ src/main/java/org/jfree/DBChartSENG275/PieChartData.java
@@ -4,40 +4,96 @@ import org.jfree.data.general.PieDataset;
 import java.sql.*;
 
 public class PieChartData {
-    /**
-     * Readings in a dataset from an existing DB
-     * @return A sample dataset.
-     * SENG 275 Project Tasks:
-     * We need to refactor this to pull out the code that creates our database;
-     * Use mocks so we can test this method without using the database!
-     * We need to test the code in here that calculates invoices per country - does it work?
-     * We should write code for testing all the CRUD operations with the database!
-     * We should write code to test what happens if the database is empty.
+
+    //Create a separate fields for DefaultPieDataset, ResultSet and Connection class
+    private static DefaultPieDataset dataset;
+    private static ResultSet resultSet;
+    private static Connection conn;
+
+    //Constructor
+    public PieChartData(DefaultPieDataset dataset, ResultSet resultSet, Connection conn){
+        this.dataset = dataset;
+        this.resultSet = resultSet;
+        this.conn = conn;
+    }
+
+    /*
+    Param: String
+    Return: Connection
+    -This method take a String as parameter to create and return a connection
+     based on the url in that String
      */
-    protected static PieDataset readChinookDataset() {
-        DefaultPieDataset dataset = new DefaultPieDataset();
-        /* Create SQLite Database Connection */
-//        Connection conn = null;
+    public static Connection getConn(String url){
+        try{
+            Connection connection = null;
+            return connection = DriverManager.getConnection(url);
+        }catch (SQLException e){
+
+        }
+        return null;
+    }
+
+    /*
+    Param: ResultSet, Connection
+    Return: resultSet after connection has been established
+    - This method create and return a resultSet after connecting to the database
+    */
+    public static ResultSet getResultSet(ResultSet resultSet, Connection conn1){
+        try{
+            Statement statement = conn1.createStatement();
+            return resultSet = statement.executeQuery("select * from Invoices" );
+        }catch (SQLException e){
+
+        }
+        return null;
+    }
+
+    /*
+    Param: DefaultPieDataset, ResultSet, Connection
+    Return: Return the dataset containing inputs from resultSet
+    -This method will read the resultSet and use its inputs to create a dataset that will
+     represent the Pie Chart based on the database
+    */
+    public static void createDataset(DefaultPieDataset dataset, ResultSet resultSet, Connection conn){
         try {
-//            // db parameters
-//            String url = "jdbc:sqlite:chinook.db";
-//            // create a connection to the database
-//            conn = DriverManager.getConnection(url);
-//            System.out.println("Connection to SQLite has been established.");
-//            Statement statement = conn.createStatement();
-//
-//            ResultSet resultSet = statement.executeQuery("select * from Invoices");
-            ResultSet resultSet = Connecting.main(new String[]{"hello world"});
             while (resultSet.next()) {
+                // country:  Category
+                // val: Categorical value. On a pie chart, the size of the slice of a category
+                //      depends on val.
                 String country = resultSet.getString("BillingCountry");
                 Double val = resultSet.getDouble("Total");
+
+                //If the dataset already contain the category (or country), then
+                //update by adding the new val to the current val in dataset
+                if (dataset.getKeys().contains(country)) {
+                    val += (double) dataset.getValue(country);
+                }
+
+                // Update the dataset with country and val
                 dataset.setValue(country, val);
             }
-
-//            conn.close();
+            //Disconnection
+            conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    // This main method will call the remaining methods in this file
+    // to create the dataset and produce the pie chart. Previously,
+    // everything is implemented inside this function. For testability
+    // purpose, we create several smaller methods and use readChinookDataset()
+    // as a wrapper that simply call other methods, without changing the behavior.
+    public static PieDataset readChinookDataset() {
+        //initialize dataset
+        dataset = new DefaultPieDataset();
+        String url = "jdbc:sqlite:chinook.db";
+
+        conn = getConn(url);                            //Get Connection
+        resultSet = getResultSet(resultSet, conn);      //Get ResultSet based on successful Connection
+        createDataset(dataset, resultSet, conn);        //Sorting and store the result in dataset.
+
+        //return dataset
         return dataset;
     }
 }
